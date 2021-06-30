2fb4a4ff81f1f7a5378b70af09bd87b5cd3de954
Antek <marmaros987@gmail.com>
2021-06-30 16:57:46 +0200

    add CalculateTag function

--- js/script.js
+++ js/script.js
@@ -52,7 +52,12 @@
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles',
     optArticleTagsSelector = '.post-tags .list',
-    optArticleAuthorSelector = '.post-author';
+    optArticleAuthorSelector = '.post-author',
+    optTagListSelector = '.tags.list',
+    optCloudClassCount = ' 5',
+    optCloudClassPrefix = 'tag-size-'
+
+
 
 
   function generateTitleLinks(customSelector = ''){
@@ -111,7 +116,57 @@
 
   generateTitleLinks();
 
+  function calculateTagsParams(tags){
+
+    const params = {
+
+      max:0,
+      min:999999
+
+    };
+
+    for(let tag in tags){
+
+      console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!' + tag + ' is used ' + tags[tag] + ' times');
+
+      if(tags[tag] > params.max){
+
+        params.max = tags[tag];
+
+      }
+
+      if(tags[tag] < params.min){
+
+        params.min = tags[tag]
+
+      }
+
+    }
+
+    return params;
+  }
+
+  function calculateTagClass(count, params){
+
+    const normalizedCount = count - params.min;
+
+    const normalizedMax = params.max - params.min;
+
+    const percentage = normalizedCount / normalizedMax;
+
+    const classNumber = Math.floor( percentage * (optCloudClassCount - 1) + 1 );
+
+    return optCloudClassPrefix+classNumber;
+
+  }
+
   function generateTags(){
+
+    /* [NEW] create a new variable allTags with an empty object */
+    let allTags = {};
+
+    console.log('stówrz tablicę ', allTags)
+
     /* find all articles */
 
     const articles = document.querySelectorAll(optArticleSelector);
@@ -160,6 +215,18 @@
 
         console.log(html);
 
+        /* [NEW] check if this link is NOT already in allTags */
+        if(!allTags[tag]) {
+          console.log('sprawdź czy link tagu znajduje się w tablicy allTags');
+
+          /* [NEW] add generated code to allTags object */
+          allTags[tag] = 1;
+
+          console.log('jeżeli link tagu nie znajduje się w obiekcie to go dodaj');
+        } else {
+          allTags[tag]++;
+        }
+
         /* END LOOP: for each tag */
 
       }
@@ -170,8 +237,48 @@
 
       /* tagsWrapper.insertAdjacentHTML('afterbegin', html); */
 
-    /* END LOOP: for every article: */
+      /* END LOOP: for every article: */
     }
+
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector('.tags');
+
+    console.log('stała tagList wyszukuje tagów w prawej kolumni (.tags)');
+
+    /* [NEW] add html from allTags to tagList */
+    // tagList.innerHTML = allTags.join(' ');
+
+    console.log('!!!!!!!!!!!!!!!!!dodaj link html z stałe allTags do tablicy tagList', allTags)
+
+    /* [NEW] create variable for all links HTML code */
+
+    const tagsParams = calculateTagsParams(allTags);
+    console.log('tagsParams:', tagsParams)
+
+    let allTagsHTML = '';
+
+    /* [NEW] START LOOP: for each tag in all tags: */
+
+    for(let tag in allTags){
+
+      /* [NEW] generate code of a link and add it to allTagsHTML */
+      const tagLinkHTML = '<li><a class="' + calculateTagClass(allTags[tag], tagsParams) + '" href="#tag-' + tag + '">' + tag + '</li>';
+
+      console.log('tagLinkHTML:', tagLinkHTML);
+
+      allTagsHTML += tagLinkHTML;
+
+     // allTagsHTML += '<li><a href="#tag-'+ tag +'">'+ tag +'</a>(' + allTags[tag] + ')</li>'
+      /* [NEW] END LOOP: for each tag in allTags: */
+    }
+
+    /* [NEW] add HTML from allTagsHTML to tagList */
+
+    tagList.innerHTML = allTagsHTML;
+
+
+
+
   }
 
 
